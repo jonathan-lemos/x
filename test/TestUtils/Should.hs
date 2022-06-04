2354d0765e18c35a87f5e670eee08fdf439fa1a5
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-06-03 21:55:36 -0400

    adding more tests

--- test/TestUtils/Should.hs
+++ test/TestUtils/Should.hs
@@ -1,6 +1,8 @@
 module TestUtils.Should where
 import Test.Hspec
 import Control.Monad
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 shouldBeSpec :: (Show a, Eq a) => String -> [(a, a, String)] -> SpecWith ()
 shouldBeSpec title as =
@@ -15,3 +17,17 @@ shouldSatisfySpec title as =
         forM_ as $ \(actual, predicate, name) ->
             it name $ do
                 actual `shouldSatisfy` predicate
+
+shouldSatisfyQcSpec :: (Arbitrary a, Show a, Show b, Eq b) => String -> [(a -> b, a -> b -> Bool, String)] -> SpecWith ()
+shouldSatisfyQcSpec title as =
+    describe title $ do
+        forM_ as $ \(func, predicate, name) ->
+            prop name $ do
+                \x -> func x `shouldSatisfy` predicate x
+
+shouldBeQcSpec :: (Arbitrary a, Show a, Show b, Eq b) => String -> [(a -> b, a -> b, String)] -> SpecWith ()
+shouldBeQcSpec title as =
+    describe title $ do
+        forM_ as $ \(actual, expected, name) ->
+            prop name $ do
+                \x -> actual x `shouldBe` expected x
--- test/Unit/UnitSpec.hs
+++ test/Unit/UnitSpec.hs
@@ -1,16 +1,44 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Unit.UnitSpec where
 
+import Data.Either
+import Data.Foldable (find)
+import Data.Maybe
 import Test.Hspec
+import TestUtils.Arbitrary
+import TestUtils.Should
+import Unit.Arithmetic
+import Unit.Prelude
 import Unit.Unit
 import Unit.UnitScaleOperation
+import Unit.UnitLike
 
 spec :: Spec
 spec = parallel $ do
-    describe "castUnit tests" $ do
-        let b = BaseUnit "b"
-        let kg = BaseUnit "kg"
-        let kb = ScaledUnit "kb" (Multiply 100) b
-        let mb = ScaledUnit "Mb" (Multiply 100) kb
+    let units = preludeUnits
+    let lookup s = fromJust $ find ((== s) . show) units
 
-        it "casts from b to kb" $ do
-            1 `shouldBe` 1
+    let right (Right x) = x
+        right (Left e) = error e
+
+    let cu a b = right $ castUnit a b
+
+    let clu a b = cu (lookup a) (lookup b)
+
+    let tc a b f = (clu a b, f, show a <> " -> " <> show b)
+
+    let anonymousNewton = (lookup "kg" `unitMult` lookup "m") `unitDiv` (lookup "s" `unitExpScalar` 2)
+
+    shouldBeSpec
+        "toScaleAndBaseUnitTests"
+        [(toScaleAndBaseUnits anonymousNewton, toScaleAndBaseUnits $ lookup "N", "anonymous newton == newton")]
+
+    shouldBeQcSpec
+        "castUnit tests"
+        [ tc "b" "kb" (/ 1000)
+        , tc "kb" "b" (* 1000)
+        , (cu anonymousNewton (lookup "N"), id, "anonymous newton -> newton")
+        , tc "K" "C" (+ 273.15)
+        , tc "K" "F" $ (* (9/5)) . (+ 32) . (+ 273.15)
+        ]
